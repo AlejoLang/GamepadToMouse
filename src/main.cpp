509495cf01490c229a4991e24e5142e2caf8586e
Alejo Lang <alejolang123@gmail.com>
2025-11-25 15:00:48 -0300

    Change code to use Gamepad class, change velocity calcs to fix sensitivity on diagonal movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,55 @@
 #include <iostream>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <string.h>
-#include <linux/uinput.h>
-#include <linux/joystick.h>
 #include "VirtualMouse.hpp"
+#include "Gamepad.hpp"
+#include "math.h"
 
 void process_event(js_event event);
+void process_event_joystick(js_event event);
+void process_event_button(js_event event);
 
-int16_t joy_x = 0;
-int16_t joy_y = 0;
 bool exit_program = false;
 
-int mouse_file;
-int gamepad_file;
-
 VirtualMouse *virtual_mouse;
+Gamepad *current_gamepad;
 
 int main()
 {
   virtual_mouse = new VirtualMouse("Test_Mouse", 0x1010, 0x0101);
-  gamepad_file = open("/dev/input/js0", O_NONBLOCK);
+  current_gamepad = new Gamepad();
 
   if (!virtual_mouse)
   {
     std::cout << "Error creating virtual mouse" << std::endl;
     return -1;
   }
-  if (!gamepad_file)
+  if (!current_gamepad)
   {
     std::cout << "Error opening gamepad" << std::endl;
     return -1;
   }
 
-  js_event event;
   while (!exit_program)
   {
-    while (read(gamepad_file, &event, sizeof(event)) > 0)
+    js_event event = current_gamepad->get_event();
+    while (event.type != static_cast<u_char>(-1))
     {
       process_event(event);
+      event = current_gamepad->get_event();
     }
-    virtual_mouse->moveMouseRelative(VirtualMouse::X_AXIS, 10 * ((float)joy_x / 32767));
-    virtual_mouse->moveMouseRelative(VirtualMouse::Y_AXIS, 10 * ((float)joy_y / 32767));
+    float x = ((float)current_gamepad->joy1.x / Gamepad::MAX_ANALOG_VALUE);
+    float y = ((float)current_gamepad->joy1.y / Gamepad::MAX_ANALOG_VALUE);
+    float mag = sqrt(x * x + y * y);
+    if (mag > 1.0f)
+    {
+      x /= mag;
+      y /= mag;
+    }
+    int dx = static_cast<int>(x * 10.f);
+    int dy = static_cast<int>(y * 10.f);
+    virtual_mouse->moveMouseRelativeXY(dx, dy);
   }
-  sleep(1);
-  close(gamepad_file);
+  delete current_gamepad;
+  delete virtual_mouse;
   return 0;
 }
 
@@ -53,27 +57,54 @@ void process_event(js_event event)
 {
   switch (event.type)
   {
-  case JS_EVENT_AXIS:
-    if (event.number == 0)
-    {
-      joy_x = event.value;
-    }
-    else if (event.number == 1)
-    {
-      joy_y = event.value;
-    }
-    std::cout << joy_x << ", " << joy_y << std::endl;
+  case Gamepad::EV_JOYSTICK:
+    process_event_joystick(event);
     break;
   case JS_EVENT_BUTTON:
-    std::cout << (int)event.number << std::endl;
-    if (event.number == 10)
-    {
-      exit_program = true;
-    }
-    if (event.number == 0)
-    {
-      event.value ? virtual_mouse->releaseButton(VirtualMouse::LEFT_CLICK) : virtual_mouse->releaseButton(VirtualMouse::LEFT_CLICK);
-    }
-    break;
+    process_event_button(event);
+  }
+}
+
+void process_event_joystick(js_event event)
+{
+  if (event.number == current_gamepad->JOY1_X)
+  {
+    current_gamepad->joy1.x = event.value;
+  }
+  else if (event.number == current_gamepad->JOY1_Y)
+  {
+    current_gamepad->joy1.y = event.value;
+  }
+  else if (event.number == current_gamepad->JOY2_X)
+  {
+    current_gamepad->joy2.x = event.value;
+  }
+  else if (event.number == current_gamepad->JOY2_Y)
+  {
+    current_gamepad->joy2.y = event.value;
+  }
+  else if (event.number == current_gamepad->DPAD_X)
+  {
+    current_gamepad->dpad.x = event.value;
+  }
+  else if (event.number == current_gamepad->DPAD_Y)
+  {
+    current_gamepad->dpad.y = event.value;
+  }
+}
+
+void process_event_button(js_event event)
+{
+  if (event.number == current_gamepad->BTN_CR)
+  {
+    event.value ? virtual_mouse->pressButton(VirtualMouse::LEFT_CLICK) : virtual_mouse->releaseButton(VirtualMouse::LEFT_CLICK);
+  }
+  else if (event.number == current_gamepad->BTN_CI)
+  {
+    event.value ? virtual_mouse->pressButton(VirtualMouse::RIGHT_CLICK) : virtual_mouse->releaseButton(VirtualMouse::RIGHT_CLICK);
+  }
+  else if (event.number == current_gamepad->BTN_RB)
+  {
+    event.value ? virtual_mouse->pressButton(VirtualMouse::MIDDLE_CLICK) : virtual_mouse->releaseButton(VirtualMouse::MIDDLE_CLICK);
   }
 }
