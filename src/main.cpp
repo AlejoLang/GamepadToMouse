0664fac197ccc198af21b7fc7770f419e0afed7e
Alejo Lang <alejolang123@gmail.com>
2025-11-25 19:02:40 -0300

    Add methods to dynamically update gamepads

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,151 @@
 #include <iostream>
+#include <vector>
+#include <filesystem>
+#include <thread>
+#include <chrono>
+#include <algorithm>
 #include "VirtualMouse.hpp"
 #include "Gamepad.hpp"
 #include "math.h"
 
+int scan_gamepads();
 void process_event(js_event event);
 void process_event_joystick(js_event event);
 void process_event_button(js_event event);
 
 bool exit_program = false;
 
-VirtualMouse *virtual_mouse;
-Gamepad *current_gamepad;
+VirtualMouse *virtual_mouse = nullptr;
+Gamepad *current_gamepad = nullptr;
+std::vector<Gamepad *> gamepads = {};
 
 int main()
 {
+  int detected_new_gamepads = scan_gamepads();
   virtual_mouse = new VirtualMouse("Test_Mouse", 0x1010, 0x0101);
-  current_gamepad = new Gamepad();
 
   if (!virtual_mouse)
   {
     std::cout << "Error creating virtual mouse" << std::endl;
     return -1;
   }
-  if (!current_gamepad)
+  if (gamepads.size() == 0)
   {
-    std::cout << "Error opening gamepad" << std::endl;
-    return -1;
+    std::cout << "Waiting for a gamepad to be connected" << std::endl;
   }
 
   while (!exit_program)
   {
-    js_event event = current_gamepad->get_event();
-    while (event.type != static_cast<u_char>(-1))
+    detected_new_gamepads = scan_gamepads();
+    if (detected_new_gamepads > 0)
     {
-      process_event(event);
-      event = current_gamepad->get_event();
+      std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-    float x = ((float)current_gamepad->joy1.x / Gamepad::MAX_ANALOG_VALUE);
-    float y = ((float)current_gamepad->joy1.y / Gamepad::MAX_ANALOG_VALUE);
-    float mag = sqrt(x * x + y * y);
-    if (mag > 1.0f)
+    if (current_gamepad)
     {
-      x /= mag;
-      y /= mag;
+      js_event event = current_gamepad->get_event();
+      while (event.type != static_cast<u_char>(-1))
+      {
+        process_event(event);
+        event = current_gamepad->get_event();
+      }
+      float x = ((float)current_gamepad->joy1.x / Gamepad::MAX_ANALOG_VALUE);
+      float y = ((float)current_gamepad->joy1.y / Gamepad::MAX_ANALOG_VALUE);
+      float mag = sqrt(x * x + y * y);
+      if (mag > 1.0f)
+      {
+        x /= mag;
+        y /= mag;
+      }
+      int dx = static_cast<int>(x * 10.f);
+      int dy = static_cast<int>(y * 10.f);
+      virtual_mouse->moveMouseRelativeXY(dx, dy);
     }
-    int dx = static_cast<int>(x * 10.f);
-    int dy = static_cast<int>(y * 10.f);
-    virtual_mouse->moveMouseRelativeXY(dx, dy);
   }
-  delete current_gamepad;
+
+  for (Gamepad *gamepad : g > amepads)
+  {
+    delete gamepad;
+  }
+
   delete virtual_mouse;
   return 0;
+}
+
+int scan_gamepads()
+{
+  int new_gamepads = 0;
+  std::vector<std::string> devices_paths;
+  // Scan the /dev/input/ files
+  for (const auto &entry : std::filesystem::directory_iterator("/dev/input/"))
+  {
+    std::string device_path = entry.path().string();
+    bool device_already_added = false;
+    // for each file, process the joysticks
+    if (device_path.find("js") != std::string::npos)
+    {
+      devices_paths.push_back(device_path);
+      // Checks if the device was already added
+      for (auto &gamepad : gamepads)
+      {
+        if (device_path == gamepad->get_path())
+        {
+          device_already_added = true;
+          break;
+        }
+      }
+
+      // If the device is new, adds it to the vector
+      if (!device_already_added)
+      {
+        new_gamepads++;
+        Gamepad *new_gamepad = new Gamepad(device_path);
+        if (new_gamepad->get_file() != -1)
+        {
+          std::cout << "Gamepad " << entry.path() << " detected" << std::endl;
+          new_gamepads++;
+          gamepads.push_back(new_gamepad);
+        }
+        else
+        {
+          std::cout << "Error processing gamepad " << entry.path() << std::endl;
+          delete new_gamepad;
+        }
+      }
+    }
+  }
+
+  // Check for devices that have been disconnected
+  for (std::vector<Gamepad *>::iterator it = gamepads.begin(); it != gamepads.end();)
+  {
+    if (find(devices_paths.begin(), devices_paths.end(), (*it)->get_path()) == devices_paths.end())
+    {
+      std::cout << "Device " << (*it)->get_path() << " removed" << std::endl;
+
+      if (current_gamepad == *it)
+      {
+        current_gamepad = nullptr;
+      }
+
+      delete (*it);
+      it = gamepads.erase(it);
+    }
+    else
+    {
+      it++;
+    }
+  }
+
+  if (current_gamepad == nullptr && !gamepads.empty())
+  {
+    current_gamepad = gamepads[0];
+  }
+  else
+  {
+    current_gamepad = nullptr;
+  }
+
+  return new_gamepads;
 }
 
 void process_event(js_event event)
