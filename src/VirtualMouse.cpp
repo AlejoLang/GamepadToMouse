19bff6b2824eed77a0d8e42858c6da0a5940cfb8
Alejo Lang <alejolang123@gmail.com>
2025-11-28 13:02:11 -0300

    Change virtual mouse to use the virtual device common class

--- src/VirtualMouse.cpp
+++ src/VirtualMouse.cpp
@@ -3,21 +3,19 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 
-VirtualMouse::VirtualMouse(std::string name, int vendor_id, int product_id)
+VirtualMouse::VirtualMouse(std::string name, int vendor_id, int product_id) : VirtualDevice("/dev/uinput")
 {
   struct uinput_setup usetup;
 
-  this->mouse_file = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
-
   /* enable mouse button left and relative events */
-  ioctl(this->mouse_file, UI_SET_EVBIT, EV_KEY);
-  ioctl(this->mouse_file, UI_SET_KEYBIT, BTN_LEFT);
-  ioctl(this->mouse_file, UI_SET_KEYBIT, BTN_RIGHT);
-  ioctl(this->mouse_file, UI_SET_KEYBIT, BTN_MIDDLE);
+  ioctl(this->device_file, UI_SET_EVBIT, EV_KEY);
+  ioctl(this->device_file, UI_SET_KEYBIT, BTN_LEFT);
+  ioctl(this->device_file, UI_SET_KEYBIT, BTN_RIGHT);
+  ioctl(this->device_file, UI_SET_KEYBIT, BTN_MIDDLE);
 
-  ioctl(this->mouse_file, UI_SET_EVBIT, EV_REL);
-  ioctl(this->mouse_file, UI_SET_RELBIT, REL_X);
-  ioctl(this->mouse_file, UI_SET_RELBIT, REL_Y);
+  ioctl(this->device_file, UI_SET_EVBIT, EV_REL);
+  ioctl(this->device_file, UI_SET_RELBIT, REL_X);
+  ioctl(this->device_file, UI_SET_RELBIT, REL_Y);
 
   memset(&usetup, 0, sizeof(usetup));
   usetup.id.bustype = BUS_USB;
@@ -25,50 +23,17 @@ VirtualMouse::VirtualMouse(std::string name, int vendor_id, int product_id)
   usetup.id.product = product_id;
   strcpy(usetup.name, name.c_str());
 
-  ioctl(this->mouse_file, UI_DEV_SETUP, &usetup);
-  ioctl(this->mouse_file, UI_DEV_CREATE);
-}
-
-void VirtualMouse::emit(int type, int code, int val)
-{
-  input_event new_event;
-
-  new_event.type = type;
-  new_event.code = code;
-  new_event.value = val;
-  new_event.time.tv_sec = 0;
-  new_event.time.tv_usec = 0;
-
-  write(this->mouse_file, &new_event, sizeof(new_event));
+  ioctl(this->device_file, UI_DEV_SETUP, &usetup);
+  ioctl(this->device_file, UI_DEV_CREATE);
 }
 
 void VirtualMouse::moveMouseRelativeXY(int dx, int dy)
 {
+  if (!dx && !dy)
+  {
+    return;
+  }
   this->emit(EV_REL, REL_X, dx);
   this->emit(EV_REL, REL_Y, dy);
-  send_sync_report();
-}
-
-void VirtualMouse::pressButton(int button)
-{
-  this->emit(EV_KEY, button, 1);
-  send_sync_report();
-}
-
-void VirtualMouse::releaseButton(int button)
-{
-  this->emit(EV_KEY, button, 0);
-  send_sync_report();
-}
-
-void VirtualMouse::send_sync_report()
-{
-  this->emit(EV_SYN, SYN_REPORT, 0);
-  usleep(10000);
-}
-
-VirtualMouse::~VirtualMouse()
-{
-  ioctl(this->mouse_file, UI_DEV_DESTROY);
-  close(this->mouse_file);
+  this->send_sync_report();
 }
--- src/VirtualMouse.hpp
+++ src/VirtualMouse.hpp
@@ -1,31 +1,30 @@
+#ifndef VirtualMouse_h
+#define VirtualMouse_h
+#include "VirtualDevice.hpp"
 #include <linux/uinput.h>
 #include <string.h>
 #include <string>
 
-#define sci static const int
-
-class VirtualMouse
+class VirtualMouse : public VirtualDevice
 {
-private:
-  int mouse_file;
-
 public:
-  sci Y_AXIS = REL_Y;
-  sci X_AXIS = REL_X;
-  sci LEFT_CLICK = BTN_LEFT;
-  sci RIGHT_CLICK = BTN_RIGHT;
-  sci MIDDLE_CLICK = BTN_MIDDLE;
-  sci SCROLL_UP = KEY_SCROLLUP;
-  sci SCROLL_DOWN = KEY_SCROLLDOWN;
-
-private:
-  void send_sync_report();
-  void emit(int type, int code, int val);
+  struct MouseKeyAction : VirtualDevice::Action
+  {
+    MouseKeyAction(int key)
+    {
+      this->device = VirtualDevice::MOUSE;
+      this->type = EV_KEY;
+      this->key = key;
+    }
+  };
+  inline static MouseKeyAction LEFT_CLICK_ACTION = MouseKeyAction(BTN_LEFT);
+  inline static MouseKeyAction RIGHT_CLICK_ACTION = MouseKeyAction(BTN_RIGHT);
+  inline static MouseKeyAction MIDDLE_CLICK_ACTION = MouseKeyAction(BTN_MIDDLE);
 
 public:
   VirtualMouse(std::string name = "Default_Name", int vendor_id = 0x1111, int product_id = 0x1111);
   void moveMouseRelativeXY(int dx, int dy);
-  void pressButton(int button);
-  void releaseButton(int button);
-  ~VirtualMouse();
+  ~VirtualMouse() {};
 };
+
+#endif
