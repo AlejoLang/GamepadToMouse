878630dcf640eab4cbbd048cd7bd4a5085b9e539
Alejo Lang <alejolang123@gmail.com>
2025-12-01 22:50:07 -0300

    Integrate new Controllers and GUI changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <filesystem>
-#include <thread>
-#include <chrono>
-#include <algorithm>
-#include "VirtualMouse.hpp"
-#include "math.h"
 #include "SDL3/SDL.h"
-#include "SDL3/SDL_gamepad.h"
-#include "SDL3/SDL_mouse.h"
-#include "GamepadController.hpp"
+#include "AppWindow.hpp"
+#include "ControllersHandler.hpp"
+#include <QtCore/QTimer>
 
 #define SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS "SDL_JOYSTICK_ALLOW_BACKGROUND_EVENTS"
 
-int scan_gamepads();
-void process_gamepad_events();
-void process_gamepad_button_event(SDL_Event *event);
+void process_gamepad_events(AppWindow *app, ControllersHandler *handler);
 
-bool exit_program = false;
-
-struct Gamepad
-{
-  SDL_JoystickID id;
-  SDL_Gamepad *gamepad;
-};
-
-VirtualMouse *virtual_mouse = nullptr;
-VirtualKeyboard *virtual_keyboard = nullptr;
-GamepadController current_controller = {nullptr, nullptr, nullptr};
-std::vector<GamepadController> controllers;
-
-int main()
+int main(int argc, char *argv[])
 {
   SDL_Init(SDL_INIT_GAMEPAD | SDL_INIT_JOYSTICK);
-  virtual_mouse = new VirtualMouse("Test_Mouse");
-  virtual_keyboard = new VirtualKeyboard("Test_Keyboard");
+  ControllersHandler *gamepads_controller_handler = new ControllersHandler();
+  AppWindow *app = new AppWindow(argc, argv, gamepads_controller_handler);
 
-  if (virtual_mouse->get_file() == -1)
-  {
-    std::cout << "Error creating virtual mouse" << std::endl;
-    return -1;
-  }
-  if (virtual_keyboard->get_file() == -1)
-  {
-    std::cout << "Error creating virtual keyboard" << std::endl;
-    return -1;
-  }
-
-  while (!exit_program)
-  {
-    process_gamepad_events();
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    if (current_controller.get_gamepad() != nullptr)
+  QTimer *timer = new QTimer();
+  QObject::connect(timer, &QTimer::timeout, [&]()
+                   {
+    process_gamepad_events(app, gamepads_controller_handler);
+    if (gamepads_controller_handler->get_current_controller() != nullptr)
     {
-      current_controller.process_joysticks();
-    }
-  }
+      gamepads_controller_handler->get_current_controller()->process_joysticks();
+    } });
 
-  for (auto controller : controllers)
-  {
-    SDL_CloseGamepad(controller.get_gamepad());
-  }
+  timer->start(4);
+  app->run();
 
-  delete virtual_mouse;
-  delete virtual_keyboard;
+  delete gamepads_controller_handler;
+  delete timer;
+  delete app;
   return 0;
 }
 
-void process_gamepad_events()
+void process_gamepad_events(AppWindow *app, ControllersHandler *handler)
 {
   SDL_Event new_event;
   bool has_event = SDL_PollEvent(&new_event);
@@ -77,44 +41,20 @@ void process_gamepad_events()
     {
     case SDL_EVENT_GAMEPAD_ADDED:
     {
-      SDL_JoystickID added_gamepad_id = new_event.gdevice.which;
-      SDL_OpenGamepad(added_gamepad_id);
-      SDL_Gamepad *added_gamepad = SDL_GetGamepadFromID(added_gamepad_id);
-      controllers.push_back({virtual_mouse, virtual_keyboard, added_gamepad});
-      if (current_controller.get_gamepad() == nullptr)
-      {
-        current_controller = GamepadController({virtual_mouse, virtual_keyboard, added_gamepad});
-        SDL_RumbleGamepad(current_controller.get_gamepad(), 0x0100, 0x0100, 10);
-      }
+      handler->add_controller(new_event.gdevice.which);
+      app->update_menu();
       break;
     }
     case SDL_EVENT_GAMEPAD_REMOVED:
     {
-      SDL_JoystickID removed_gamepad_id = new_event.gdevice.which;
-      for (std::vector<GamepadController>::iterator it = controllers.begin(); it != controllers.end(); it++)
-      {
-        if (it->get_gamepad_id() == removed_gamepad_id)
-        {
-          SDL_CloseGamepad(it->get_gamepad());
-          controllers.erase(it);
-          break;
-        }
-      }
-      if (current_controller.get_gamepad_id() == removed_gamepad_id)
-      {
-        current_controller = controllers.empty() ? GamepadController(nullptr, nullptr, nullptr) : controllers[0];
-      }
+      handler->remove_controller(new_event.gdevice.which);
+      app->update_menu();
       break;
     }
     case SDL_EVENT_GAMEPAD_BUTTON_DOWN:
     case SDL_EVENT_GAMEPAD_BUTTON_UP:
     {
-      if (new_event.gbutton.button == SDL_GAMEPAD_BUTTON_GUIDE)
-      {
-        exit_program = 1;
-        return;
-      }
-      current_controller.process_key_event(&new_event);
+      handler->get_current_controller()->process_key_event(&new_event);
       break;
     }
     default:
